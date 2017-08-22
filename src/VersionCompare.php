f4ba7679a81157c8d1a756d3a16c5619cfccf516
puzhengchao <zhengchaopu@gmail.com>
2017-08-22 23:52:17 +0800

    Update VersionCompare

--- src/VersionCompare.php
+++ src/VersionCompare.php
@@ -2,38 +2,163 @@
 
 namespace Emanci\VersionCompare;
 
+/**
+ * @link http://semver.org Semantic Versioning
+ */
 class VersionCompare
 {
     /**
-     * Compares two "PHP-standardized" version number strings.
+     * The major version number.
      *
-     * @param string $version1
-     * @param string $operator
-     * @param string $version2
-     *
-     * @return bool
+     * @var int
      */
+    protected $major = 0;
+
+    /**
+     * The minor version number.
+     *
+     * @var int
+     */
+    protected $minor = 0;
+
+    /**
+     * The patch number.
+     *
+     * @var int
+     */
+    protected $patch = 0;
+
+    /**
+     * The pre-release version number.
+     *
+     * @var mixed
+     */
+    protected $preRelease;
+
+    /**
+     * The build metadata information.
+     *
+     * @var mixed
+     */
+    protected $buildMetadata;
+
+    protected $comps = [
+        -1 => '<',
+        0 => '==',
+        1 => '>',
+    ];
+
+    /**
+     * VersionCompare construct.
+     *
+     * @param string $version
+     */
+    public function __construct($version = null)
+    {
+        if ($version && is_string($version)) {
+            $this->parseVersion($version);
+        }
+    }
+
+    public function setMajor($major)
+    {
+        $this->major = $major;
+    }
+
+    public function setMinor($minor)
+    {
+        $this->minor = $minor;
+    }
+
+    public function setPatch($patch)
+    {
+        $this->patch = $patch;
+    }
+
+    public function setPreRelease($preRelease)
+    {
+        $this->preRelease = $preRelease;
+    }
+
+    public function setBuildMetadata($buildMetadata)
+    {
+        $this->buildMetadata = $buildMetadata;
+    }
+
+    public function getMajor()
+    {
+        return $this->major;
+    }
+
+    public function getMinor()
+    {
+        return $this->minor;
+    }
+
+    public function getPatch()
+    {
+        return $this->patch;
+    }
+
+    public function getPreRelease()
+    {
+        return $this->preRelease;
+    }
+
+    public function getBuildMetadata()
+    {
+        return $this->buildMetadata;
+    }
+
+    /**
+     * Creates a new Version instance.
+     *
+     * @param string $version
+     *
+     * @return Version
+     */
+    public static function create($version)
+    {
+        return new static($version);
+    }
+
     public function compare($version1, $operator, $version2)
     {
-        $newVersion1 = trim(str_replace('.', '', $version1));
-        $newVersion2 = trim(str_replace('.', '', $version2));
+        $version1 = static::create($version1);
+        $version2 = static::create($version2);
 
-        if (is_numeric($newVersion1) && is_numeric($newVersion2)) {
-            $newVersion1Len = strlen($newVersion1);
-            $newVersion2Len = strlen($newVersion2);
+        $version1Str = strval($version1->getMajor().$version1->getMinor().$version1->getPatch());
+        $version2Str = strval($version2->getMajor().$version2->getMinor().$version2->getPatch());
 
-            if ($newVersion1Len > $newVersion2Len) {
-                $newVersion2 = str_pad($newVersion2, $newVersion1Len, 0);
-            }
-
-            if ($newVersion1Len < $newVersion2Len) {
-                $newVersion1 = str_pad($newVersion1, $newVersion2Len, 0);
-            }
-        } else {
-            $newVersion1 = trim($version1);
-            $newVersion2 = trim($version2);
+        if ($comp = version_compare($version1Str, $version2Str)) {
+            return $this->comps[$comp] == $operator;
         }
 
-        return version_compare((string) $newVersion1, (string) $newVersion2, $operator);
+        // continue
+    }
+
+    protected function parseVersion($version)
+    {
+        if (false !== strpos($version, '+')) {
+            list($version, $buildMetadata) = explode('+', $version);
+            $this->setBuildMetadata($buildMetadata);
+        }
+
+        if (false !== strpos($version, '-')) {
+            list($version, $preRelease) = explode('-', $version);
+            $this->setPreRelease($preRelease);
+        }
+
+        $versions = explode('.', $version);
+
+        $this->setMajor($versions[0]);
+
+        if (isset($versions[1])) {
+            $this->setMinor($versions[1]);
+        }
+
+        if (isset($versions[2])) {
+            $this->setPatch($versions[2]);
+        }
     }
 }
